d7dfbbbc70ec2d177ed80bb8adc905661373f4fd
pritee <pritikyada22@gmail.com>
2023-12-10 00:19:38 +0530

    complate

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sliverappbar/Screen/calls_screen.dart';
 import 'package:sliverappbar/Screen/chat_screen.dart';
 import 'package:sliverappbar/Screen/group_screen.dart';
 import 'package:sliverappbar/Screen/update_screen.dart';
 
 
-
+//
 class SliverAppBarDemo extends StatefulWidget {
   const SliverAppBarDemo({super.key});
 
@@ -16,11 +17,15 @@ class SliverAppBarDemo extends StatefulWidget {
 class _SliverAppBarDemoState extends State<SliverAppBarDemo>
     with SingleTickerProviderStateMixin {
   TabController? _controller;
+  final ScrollController _innerScrollController = ScrollController();
   @override
   void initState() {
     print("hello");
     super.initState();
     _controller = TabController(length: 4, vsync: this, initialIndex: 1);
+    _innerScrollController.addListener(() {
+      print('Scroll position: ${_innerScrollController.offset}');
+    });
   }
 
   List<Widget> list = [
@@ -31,24 +36,45 @@ class _SliverAppBarDemoState extends State<SliverAppBarDemo>
   ];
   List<Widget> screen = [
     const GroupScreen(),
-    const ChatPage(),
+     ChatPage(),
     const UpdateScreen(),
     const CallsScreen(),
   ];
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: NestedScrollView(
         headerSliverBuilder: (context, innerBoxIsScrolled) => [
-          SliverAppBar(
-            pinned: true,
-            floating: true,
+          SliverAppBar
+            (
+            //expandedHeight: 400,
+            //expandedHeight: ,
+           // centerTitle: true,//use for center place your title
+            //clipBehavior: Clip.antiAliasWithSaveLayer,
+           // titleTextStyle: used when you set default property in flutter ,
+           // shadowColor: Colors.black38,
+           // surfaceTintColor: Colors.red,
+           // automaticallyImplyLeading: false,
+            //collapsedHeight: 100 ,//appbar work  as simple appbar
+            //excludeHeaderSemantics: true,
+            forceElevated: true,//used for you don't want to change colour of the appbar when the inner widget scroll use that property
+            //forceMaterialTransparency: true,//appbar colour is white
+           // stretch:true ,
+                //snap: true,
+            //foregroundColor: Colors.black,
+            systemOverlayStyle: SystemUiOverlayStyle.light,
+
+            //elevation: 12,
+            //flexibleSpace: Icon(Icons.account_circle_sharp),
+            pinned: true,//used for the hold appbar when inner screen scrolled
+            floating: true,//used for enimation of the sliver appbar
             iconTheme: const IconThemeData(color: Colors.white),
             toolbarHeight: 60,
-            backgroundColor: Colors.teal.shade900,
+            backgroundColor: Colors.teal.shade900,//dont used tansperent colour
             //const Color(0xff0D5B53),
-            leadingWidth: 140,
+           // leadingWidth: 10,
             title: const Text("WhatsApp",
                 style: TextStyle(fontSize: 30, color: Colors.white),
                 textAlign: TextAlign.center),
